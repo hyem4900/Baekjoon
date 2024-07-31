54d07074ed449fa958d09ad23d02dc90db68b00e
김혜민 <156631697+hyem4900@users.noreply.github.com>
2024-08-01 00:54:01 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 256 ms, Memory: 112876 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -2,25 +2,27 @@ from heapq import heappop, heappush
 inf = 1e4
 
 
-def sol(arr, cnt, n, x=0, y=0):
-    for dx, dy in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
-        nx, ny = x + dx, y + dy
-        if nx in range(n) and ny in range(n):
-            tmp = dp[x][y] + arr[nx][ny]
-            if tmp >= dp[nx][ny]:continue
-            dp[nx][ny] = tmp
-            heappush(q, (tmp, nx, ny))
 
-i = 1
+tc = 0
 while True:
-    n = int(input())
-    if not n:exit()
-    dp, arr = [[inf] * n for _ in range(n)], [list(map(int, input().split())) for _ in range(n)]
+    tc += 1
+    N = int(input())
+    if not N:
+        exit()
+    arr = [list(map(int,input().split())) for n in range(N)]
+    dir = [(1,0),(0,1),(-1,0),(0,-1)]
+    dp = [[inf]*N for _ in range(N)]
     dp[0][0] = arr[0][0]
     q = []
-    heappush(q, (dp[0][0], 0, 0))   # 요금, 좌표
+    heappush(q,(arr[0][0], 0, 0))
     while q:
-        cost, x, y = heappop(q)
-        sol(arr, cost, n, x, y)
-    print(f'Problem {i}: {dp[-1][-1]}')
-    i += 1
+        c, i, j = heappop(q)
+        for di, dj in dir:
+            ni = i + di
+            nj = j + dj
+            if ni in range(N) and nj in range(N):
+                nc = c + arr[ni][nj]
+                if dp[ni][nj] > nc:
+                    dp[ni][nj] = nc
+                    heappush(q, (nc, ni ,nj))
+    print(f'Problem {tc}: {dp[N-1][N-1]}')
