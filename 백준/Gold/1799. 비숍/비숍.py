bcae349413d1d3cbd9cbd26ba2867649916ce492
김혜민 <156631697+hyem4900@users.noreply.github.com>
2024-08-10 17:12:08 +0900

    [Gold I] Title: 비숍, Time: 452 ms, Memory: 120192 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.py
+++ 백준/Gold/1799. 비숍/비숍.py
@@ -2,33 +2,19 @@
 import sys
 input = sys.stdin.readline
 
-def B_dfs(n, s):
-    global B_ans
-    if B_ans >= n + len(B_lst) - s:
+def dfs(n, s, lst):
+    global ans
+    if ans >= n + len(lst) - s:
         return
-    B_ans = max(B_ans, n)
-    for k in range(s, len(B_lst)):
-        i, j = B_lst[k]
-        if not B_used1[i + j] and not B_used2[i - j]:
-            B_used1[i + j] = 1
-            B_used2[i - j] = 1
-            B_dfs(n + 1, k + 1)
-            B_used1[i + j] = 0
-            B_used2[i - j] = 0
-
-def W_dfs(n, s):
-    global W_ans
-    if W_ans >= n + len(W_lst) - s:
-        return
-    W_ans = max(W_ans, n)
-    for k in range(s, len(W_lst)):
-        i, j = W_lst[k]
-        if not W_used1[i + j] and not W_used2[i - j]:
-            W_used1[i + j] = 1
-            W_used2[i - j] = 1
-            W_dfs(n + 1, k + 1)
-            W_used1[i + j] = 0
-            W_used2[i - j] = 0
+    ans = max(ans, n)
+    for k in range(s, len(lst)):
+        i, j = lst[k]
+        if not used1[i + j] and not used2[i - j]:
+            used1[i + j] = 1
+            used2[i - j] = 1
+            dfs(n + 1, k + 1, lst)
+            used1[i + j] = 0
+            used2[i - j] = 0
 
 N = int(input()) # 체스판의 크기
 arr = [list(map(int,input().split())) for _ in range(N)]
@@ -42,13 +28,11 @@ for i in range(N):
             else:
                 W_lst.append((i, j))
 
-B_used1 = [0] * (N*2-1) # i + j 우상향
-B_used2 = [0] * (N*2-1) # i - j 우하향
-W_used1 = [0] * (N*2-1)
-W_used2 = [0] * (N*2-1)
-B_ans = 0
-W_ans = 0
-B_dfs(0, 0)
-W_dfs(0, 0)
-
-print(B_ans + W_ans)
+used1 = [0] * (N*2-1) # i + j 우상향
+used2 = [0] * (N*2-1) # i - j 우하향
+ans = 0
+dfs(0, 0, B_lst)
+res = ans
+ans = 0
+dfs(0, 0, W_lst)
+print(res + ans)
